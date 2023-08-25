61749d363c559e67a4ac0313980040074c288993
Brayegh1 <brayegh1@gmail.com>
2023-08-25 08:08:52 -0700

    exits

---
+++ Gexit.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - Used to copy a string
+ *@dest: Where the string is being copied to
+ *@src: the source string
+ *@n: the number of Chars to be copied
+ *Return: The Concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - Joins two strings
+ *@dest: The First String
+ *@src: The Second String
+ *@n: the size of bytes to be Maximally used
+ *Return: The Concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - Locates a Char in a strings.
+ *@s: A string to be parsed
+ *@c: the Char to look for
+ *Return: (s) which is  pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
